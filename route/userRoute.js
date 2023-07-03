c96cf48c7191e1fd52485ff7214b4b95001431f4
Favour Ebrusike <ebrusikefavour@gmail.com>
2023-07-04 00:53:18 +0200

    first push

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { login, signUp,createSendToken } = require("../controller/userController");
+const { login, signUp } = require("../controller/userController");
 
 const Router = express.Router();
 
 Router.route("/login").post(login);
 Router.route("/signup").post(signUp);
-Router.route("/createSendToken").post(createSendToken);
 
 module.exports = Router;
